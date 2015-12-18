8e03ebba66a160612468cf454e642bc9bac48f61
lmstearn <lmstearn@ozemail.com.au>
2015-12-18 14:47:06 +1100

    Update CreateLargeDir64.h

--- CreateLargeDir64.h
+++ CreateLargeDir64.h
@@ -41,6 +41,26 @@
 #endif
 #endif
 
+
+//NT_ERROR(ntStatus)) bad practice to define consts here (performance)
+#define STATUS_OBJECT_NAME_INVALID			((NTSTATUS)0xC0000033L)
+#define STATUS_OBJECT_NAME_COLLISION		((NTSTATUS)0xC0000035L)
+#define STATUS_OBJECT_PATH_INVALID			((NTSTATUS)0xC0000039L)
+#define STATUS_OBJECT_PATH_SYNTAX_BAD		((NTSTATUS)0xC000003BL)
+#define STATUS_DATA_OVERRUN					((NTSTATUS)0xC000003CL)
+#define STATUS_DATA_LATE_ERROR				((NTSTATUS)0xC000003DL)
+#define STATUS_DATA_ERROR					((NTSTATUS)0xC000003EL)
+#define STATUS_CRC_ERROR					((NTSTATUS)0xC000003FL)
+#define STATUS_NO_GUID_TRANSLATION			((NTSTATUS)0xC000010CL)
+#define STATUS_OPEN_FAILED					((NTSTATUS)0xC0000136L)
+#define STATUS_NOT_A_REPARSE_POINT			((NTSTATUS)0xC0000275L)
+#define STATUS_DIRECTORY_IS_A_REPARSE_POINT	((NTSTATUS)0xC0000281L)
+#define STATUS_CANNOT_MAKE					((NTSTATUS)0xC00002EAL)
+					
+						
+
+
+
 #ifndef WINDOWS_NTSTATUS_H
 #define WINDOWS_NTSTATUS_H
 
@@ -48,13 +68,21 @@
 * @author Roger Karlsson
 * @since 2009-03-13
 */
-
+//First (highest order) 2 bits of 32 bit NTSATUS = severity code
+//3rd  bit is customer code
+//4 - 16 is "Facility": what facility generated the error
+//17-32 is actual error code of above facility
 #ifndef NT_SUCCESS
-#define NT_SUCCESS(Status) ((NTSTATUS)(Status) >= 0)
+#define NT_SUCCESS(Status) ((NTSTATUS)(Status) >= 0) //((NTSTATUS)(Status) >= 0) is function
 #endif
-
+#ifndef NT_INFORMATION
+#define NT_INFORMATION(Status) (((ULONG)(Status) >> 30) == 1) //easiest to bitwise shift 30 to get severity code
+#endif
+#ifndef NT_WARNING
+#define NT_WARNING(Status) (((ULONG)(Status) >> 30) == 2)
+#endif
 #ifndef NT_ERROR
-#define NT_ERROR(Status) ((ULONG)(Status) >> 30 == 3)
+#define NT_ERROR(Status) (((ULONG)(Status) >> 30) == 3)
 #endif
 
 #endif //WINDOWS_NTSTATUS_H
